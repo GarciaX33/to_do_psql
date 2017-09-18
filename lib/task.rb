0c28fd0744d869dff078a450fa26775f4773c2e7
daniel garcia and senya webster <daniel@>
2017-09-18 16:14:06 -0700

    finish up backend and add 11th spec for the Order_By

--- lib/task.rb
+++ lib/task.rb
@@ -1,28 +1,30 @@
 
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date)
 
   def initialize(attributes)
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date)
   end
 
   def self.all
-    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    returned_tasks = DB.exec("SELECT * FROM tasks ORDER BY due_date;")
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date").to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
 
   def save
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});") # do we need to add @due_date and/or @category?
   end
 
   def ==(another_task)
-    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id())) # do we need to add @due_date and/or @category?
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -7,9 +7,19 @@ describe(Task) do
     end
   end
 
+  describe(".all") do
+    it("will order tasks by due date") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-17"})
+      task1.save()
+      task2.save()
+      expect(Task.all).to(eq([task2, task1]))
+    end
+  end
+
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -17,22 +27,22 @@ describe(Task) do
 
   describe("#description") do
     it("lets you read the description out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
       expect(test_task.description()).to(eq("learn SQL"))
     end
   end
 
   describe("#list_id") do
     it("lets you read the list ID out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
   describe("#==") do
     it("is the same task if it has the same description and list ID") do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2017-9-18"})
       expect(task1).to(eq(task2))
     end
   end
